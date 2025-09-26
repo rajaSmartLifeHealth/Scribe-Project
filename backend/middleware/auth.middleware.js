49ca117a2f39bf26adbb5a7a5644651d1baa4bfe
rajaSmartLifeHealth <raja.sekhar@smartlifehealth.co.uk>
2025-09-26 16:52:35 +0530

    middlewaree expired error fixed

--- backend/middleware/auth.middleware.js
+++ backend/middleware/auth.middleware.js
@@ -1,31 +1,30 @@
-const jwt = require('jsonwebtoken');
-const { BlackTokenModel } = require('../models/token.models');
-const e = require('express');
-const { raw } = require('body-parser');
+const jwt = require("jsonwebtoken");
+const { BlackTokenModel } = require("../models/token.models");
 
-const auth = async(req,res, next)=>{
-
+const auth = async (req, res, next) => {
+  try {
     const token = req.headers.authorization?.split(" ")[1];
 
-     const blacklisted = await BlackTokenModel.findOne({ blackToken: token });
+    if (!token) {
+      return res.status(401).json({ msg: "Please login, no token provided" });
+    }
 
+    // check if token is blacklisted
+    const blacklisted = await BlackTokenModel.findOne({ where: { blackToken: token } });
     if (blacklisted) {
       return res.status(401).json({ msg: "Session logged out. Please log in again." });
     }
 
-    if(token){
-       const decoded = jwt.verify(token,"masai");
-            if(decoded){
-                req.clinicianId = decoded.userID;
-                next();
-            } else {
-                res.send({msg: "not authorized no access", "error":err});
-            }
-        }
-    else {
-       res.send({msg: "please login"});
+    // verify token
+    const decoded = jwt.verify(token, process.env.JWT_SECRET || "masai");
+    req.clinicianId = decoded.userID;
+    next();
+  } catch (err) {
+    if (err.name === "TokenExpiredError") {
+      return res.status(401).json({ msg: "Token expired. Please log in again." });
     }
-}
-module.exports = {
-    auth
-}
+    return res.status(401).json({ msg: "Not authorized, invalid token", error: err.message });
+  }
+};
+
+module.exports = { auth };
