6496a7a9118e977d2c908d5685c65c58e9b70bcf
rajaSmartLifeHealth <raja.sekhar@smartlifehealth.co.uk>
2025-10-14 18:24:26 +0530

    express json limit increased

--- backend/index.js
+++ backend/index.js
@@ -25,7 +25,7 @@ app.get('/', (req,res)=>{
     res.send({"msg": "welcome to the server"});
 })
 
-app.use(express.json());
+app.use(express.json({limit : '10mb'}));
 // app.use(cors());
 app.use('/users',userRouter);
 app.use('/api/ai', auth, searchRouter);
